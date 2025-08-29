51fb816f650c28ade8a252f97295e1c14981a6ef
Karna4571 <karna4571@gmail.com>
2025-08-29 10:44:00 +0530

    Add files via upload

---
+++ Bajaj/src/main/java/com/bajaj/WebhookSqlSolverApplication.java
@@ -0,0 +1,25 @@
+package com.bajaj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+@SpringBootApplication
+public class WebhookSqlSolverApplication {
+
+    private final WebhookService webhookService;
+
+    public WebhookSqlSolverApplication(WebhookService webhookService) {
+        this.webhookService = webhookService;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebhookSqlSolverApplication.class, args);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void onApplicationReady() {
+        webhookService.processWebhookFlow();
+    }
+} 
