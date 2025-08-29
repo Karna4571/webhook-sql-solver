d11f4716b78fdb069d303eea9762ec62e179e624
Karna4571 <karna4571@gmail.com>
2025-08-29 10:59:00 +0530

    Delete Bajaj directory

--- Bajaj/src/main/java/com/bajaj/repository/SqlResultRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bajaj.repository;
-
-import com.bajaj.entity.SqlResult;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SqlResultRepository extends JpaRepository<SqlResult, Long> {
-} 
